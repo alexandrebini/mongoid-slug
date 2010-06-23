e12eebaa83cf7b08bfced29f20618672ad80c340
Alexandre Bini <alexandre@voraz.com.br>
2010-06-23 14:55:26 -0300

    :as option

--- lib/mongoid_slug.rb
+++ lib/mongoid_slug.rb
@@ -2,43 +2,53 @@
 module Mongoid::Slug
   
   def self.included(base)
+    class << base
+      attr_accessor :slugged_field
+      attr_accessor :slugged_source
+    end
     base.extend ClassMethods
-    base.class_eval { field :slug; before_save :slugify }
   end
 
   module ClassMethods #:nodoc:
     
     # Set a field as source of slug
-    def slug(field)
-      class_variable_set(:@@slugged_field, field.to_sym)
+    def slug(field, options={})
+      self.slugged_field = options[:as] || :slug
+      self.slugged_source = field
+      field self.slugged_field, :type => String
+      before_save :slugify
     end
 
     def find_by_slug(slug)
-      where(:slug => slug).first
+      where(self.slugged_field => slug).first
     end
   end
 
   def to_param
-    slug
+    read_attribute(slugged_field)
   end
 
   private
 
   def slugify
-    self.slug = find_unique_slug if new_record? || self.send((self.class.class_eval('@@slugged_field').to_s + '_changed?').to_sym)
+    self.send "#{slugged_field.to_s}=", find_unique_slug if new_record? || self.send((slugged_source.to_s + '_changed?').to_sym)
   end
 
   def find_unique_slug(suffix='')
-    slug = ("#{slugged_field} #{suffix}").parameterize
-    if collection.find(:slug => slug).count == 0
+    slug = ("#{read_attribute(slugged_source)} #{suffix}").parameterize
+
+    if collection.find(slugged_source => slug).count == 0
       slug
     else
       new_suffix = suffix.blank? ? '1' : "#{suffix.to_i + 1}"
       find_unique_slug(new_suffix)
     end
   end
-
+  
   def slugged_field
-    self.send(self.class.class_eval('@@slugged_field'))
+    self.class.slugged_field
+  end
+  def slugged_source
+    self.class.slugged_source
   end
 end
